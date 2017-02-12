01234e1d705bc0afdbf1d924da27449431601a48
ChaneyZorn <chaneyzorn@gmail.com>
2017-02-13 00:39:44 +0800

    MathJax公式、js脚本分离

---
+++ themes/moony/static/js/load.js
@@ -0,0 +1,58 @@
+hljs.initHighlightingOnLoad();
+
+SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '{{ .Site.BaseURL }}/search.json',
+    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+    noResultsText: 'No results found',
+    limit: 10,
+    fuzzy: false,
+    exclude: ['Welcome']
+})
+
+$('.ui.dropdown').dropdown({
+    on: 'hover'
+});
+
+$(window).on('scroll', function() {
+    $('.back-top').toggleClass('show-on', window.pageYOffset > 50);
+});
+
+$('.back-top').on('click', function() {
+    $('body').velocity("scroll", {
+        duration: 1000,
+        easing: [25, 10]
+    });
+});
+
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [
+            ['$', '$']
+        ],
+        displayMath: [
+            ['$$', '$$']
+        ],
+        processEscapes: true,
+        processEnvironments: true,
+        skipTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+        TeX: {
+            equationNumbers: {
+                autoNumber: "AMS"
+            },
+            extensions: ["AMSmath.js", "AMSsymbols.js"]
+        }
+    }
+});
+
+MathJax.Hub.Queue(function() {
+    // Fix <code> tags after MathJax finishes running. This is a
+    // hack to overcome a shortcoming of Markdown. Discussion at
+    // https://github.com/mojombo/jekyll/issues/199
+    var all = MathJax.Hub.getAllJax(),
+        i;
+    for (i = 0; i < all.length; i += 1) {
+        all[i].SourceElement().parentNode.className += 'mathjax';
+    }
+});
