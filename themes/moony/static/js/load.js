451f3dfee7bfab084d0a58a770fb70efb84ad370
ChaneyZorn <chaneyzorn@gmail.com>
2017-02-28 22:44:44 +0800

    多说评论

--- themes/moony/static/js/load.js
+++ themes/moony/static/js/load.js
@@ -80,3 +80,16 @@ MathJax.Hub.Queue(function() {
         all[i].SourceElement().parentNode.className += 'mathjax';
     }
 });
+
+if ($(".ds-thread").length != 0) {
+    var duoshuoQuery = { short_name: "campanula" };
+    (function() {
+        var ds = document.createElement('script');
+        ds.type = 'text/javascript';
+        ds.async = true;
+        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+        ds.charset = 'UTF-8';
+        (document.getElementsByTagName('head')[0] ||
+            document.getElementsByTagName('body')[0]).appendChild(ds);
+    })();
+}
