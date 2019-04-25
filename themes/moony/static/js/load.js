3d612337d8fb43dfcdbc6876ebfa740ac60f8448
chaney <chaneyzorn@gmail.com>
2019-04-25 19:49:13 +0800

    修正footer

--- themes/moony/static/js/load.js
+++ themes/moony/static/js/load.js
@@ -80,17 +80,3 @@ MathJax.Hub.Queue(function() {
         all[i].SourceElement().parentNode.className += 'mathjax';
     }
 });
-
-// 多说评论
-if ($(".ds-thread").length != 0 || $(".ds-thread-count").length != 0) {
-    var duoshuoQuery = { short_name: "campanula" };
-    (function() {
-        var ds = document.createElement('script');
-        ds.type = 'text/javascript';
-        ds.async = true;
-        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-        ds.charset = 'UTF-8';
-        (document.getElementsByTagName('head')[0] ||
-            document.getElementsByTagName('body')[0]).appendChild(ds);
-    })();
-}
